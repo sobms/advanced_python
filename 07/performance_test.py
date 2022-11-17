dc1bc6a61187e26acb5790eb98aad78ef9c1f28e
sobms <sob.mixail2017@yandex.ru>
2022-11-18 00:05:53 +0300

    new gitignore

--- 07/performance_test.py
+++ 07/performance_test.py
@@ -3,6 +3,7 @@
 import time
 import cutils
 
+
 def matrix_multiplication(*args):
     matrix_a = args[0]
     result = []
@@ -12,30 +13,44 @@ def matrix_multiplication(*args):
         for j in range(len(matrix_a)):
             row = []
             for k in range(len(matrix_b[0])):
-                row.append(sum([matrix_a[j][t] * matrix_b[t][k] 
-                    for t in range(len(matrix_a[j]))]))
+                row.append(
+                    sum(
+                        [
+                            matrix_a[j][t] * matrix_b[t][k]
+                            for t in range(len(matrix_a[j]))
+                        ]
+                    )
+                )
             result.append(row)
         matrix_a = result
     return result
-                
 
 
 def main():
-    args = ([[(3*i+j)/10 for i in range(100)] for j in range(50)], 
-            [[(2*i+j)/20 for i in range(100)] for j in range(100)],
-            *[[[(i+2*j)/20 for i in range(100)] for j in range(100)] 
-            for _ in range(100)])
+    args = (
+        [[(3 * i + j) / 10 for i in range(100)] for j in range(50)],
+        [[(2 * i + j) / 20 for i in range(100)] for j in range(100)],
+        *[
+            [[(i + 2 * j) / 20 for i in range(100)] for j in range(100)]
+            for _ in range(100)
+        ],
+    )
     print("+---------- python ----------+")
     start_time = time.time()
     result = matrix_multiplication(*args)
     end_time = time.time()
-    print(f"Time of execution of python matrix multiplication is {end_time-start_time} seconds")
+    print(
+        f"Time of execution of python matrix multiplication is {end_time-start_time} seconds"
+    )
     print("+---------- capi ----------+")
     start_time = time.time()
     result_capi = cutils.matrix_multiplication(*args)
     end_time = time.time()
-    print(f"Time of execution of capi matrix multiplication is {end_time-start_time} seconds")
+    print(
+        f"Time of execution of capi matrix multiplication is {end_time-start_time} seconds"
+    )
     assert result == result_capi
+
 
 if __name__ == "__main__":
     main()
--- 07/setup.py
+++ 07/setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, Extension
 
 setup(
-    name = 'cutils',
-    version = '1.0',
-    description = 'Example of C extension',
-    author = 'Anton Kukhtichev',
-
-    ext_modules = [
-        Extension('cutils', ['cutils.cpp'])
-    ]
+    name="cutils",
+    version="1.0",
+    description="Example of C extension",
+    author="Anton Kukhtichev",
+    ext_modules=[Extension("cutils", ["cutils.cpp"])],
 )
