8002339abf65cf662696b41fd77d703feddd21ca
sobms <sob.mixail2017@yandex.ru>
2022-11-20 20:12:33 +0300

    hw07new_version

--- 07/setup.py
+++ 07/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 setup(
     name="cutils",
     version="1.0",
-    description="Example of C extension",
-    author="Anton Kukhtichev",
+    description="C extension for matrix multiplication",
+    author="Sobolev Mikhail",
     ext_modules=[Extension("cutils", ["cutils.cpp"])],
 )
